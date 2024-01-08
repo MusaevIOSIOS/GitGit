f89051b2034025d78a961577697ee53204a12a4f
beksultanmusaev <musaeviosdev@gmail.com>
2024-01-08 20:46:27 +0600

    Create registration screen

--- GitGit/ViewController.swift
+++ GitGit/ViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var age: Int = 29
+    
+    var age2: Int = 45
 
     override func viewDidLoad() {
         super.viewDidLoad()
